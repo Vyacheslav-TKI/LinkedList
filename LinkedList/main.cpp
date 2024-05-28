31e58323d75022f26935ad24b5185bbb617e0cee
I461-TKI <lukankinv20@gmail.com>
2024-05-28 15:54:02 +0300

    сделал main

--- LinkedList/main.cpp
+++ LinkedList/main.cpp
@@ -1,3 +1,7 @@
+#include <iostream>
+#include "..\Solver\Circular_linked_list.h"
+#include "..\Solver\Node.h"
+
 /**
 * @brief Точка входа в программу
 * @return 0 Программа выполнена успешно
@@ -5,5 +9,12 @@
 
 int main()
 {
+	rut::miit::llist::Circular_linked_list list = { 1, 2, 3, 4 };
+
+	list.insert(5);
+	list.remove(3);
+
+	std::cout << list << std::endl;
+
 	return 0;
 }
